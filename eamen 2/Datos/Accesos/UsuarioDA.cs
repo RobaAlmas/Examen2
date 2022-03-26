7d588c3443f603643a01868f928f4a10b56694b7
RobaAlmas <98668345+RobaAlmas@users.noreply.github.com>
2022-03-26 02:03:33 -0600

    33

---
+++ eamen 2/Datos/Accesos/UsuarioDA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Accesos
+{
+    public class UsuarioDA
+    {
+        readonly string cadena = "Server=localhost; Port=3306; Database=Examen2; Uid=root; Pwd=5050;";
+
+    }
+}
